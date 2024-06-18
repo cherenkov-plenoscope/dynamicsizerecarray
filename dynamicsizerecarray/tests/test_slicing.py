350b3b5e6b8c2543e74c5e4af5a346bf7154dc4c
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-06-18 13:32:22 +0200

    test slicing

---
+++ dynamicsizerecarray/tests/test_slicing.py
@@ -0,0 +1,22 @@
+import dynamicsizerecarray
+import pytest
+
+
+def test_slice():
+    dra = dynamicsizerecarray.DynamicSizeRecarray(
+        dtype=[("a", "i8"), ("b", "u2")]
+    )
+
+    for i in range(100):
+        dra.append_record({"a": 2 * i, "b": i})
+
+    with pytest.raises(IndexError):
+        _ = dra[200]
+
+    s = dra[10:20]
+    for idx, val in enumerate(range(10, 20)):
+        assert s["b"][idx] == val
+
+    s = dra[10:2:20]
+    for idx, val in enumerate(range(10, 2, 20)):
+        assert s["b"][idx] == val
