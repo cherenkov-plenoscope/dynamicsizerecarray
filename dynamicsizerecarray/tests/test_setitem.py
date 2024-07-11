c534a1373764742bba3460a06b4a0b8a8043e3bb
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-07-11 12:48:27 +0200

    add tests for setitem

---
+++ dynamicsizerecarray/tests/test_setitem.py
@@ -0,0 +1,38 @@
+import dynamicsizerecarray
+import numpy as np
+
+
+def make_test_dynrec():
+    r = dynamicsizerecarray.DynamicSizeRecarray(
+        dtype=[("a", "i8"), ("b", "u2")]
+    )
+    r.append_record({"a": 3, "b": 10})
+    r.append_record({"a": 2, "b": 20})
+    r.append_record({"a": 1, "b": 30})
+
+    assert r._recarray.shape[0] > len(
+        r
+    )  # make sure there is overhead capacity
+    return r
+
+
+def test_setitem_by_str():
+    r = make_test_dynrec()
+
+    np.testing.assert_array_equal(r["a"], [3, 2, 1])
+    r["a"] = [2, 4, 8]
+    np.testing.assert_array_equal(r["a"], [2, 4, 8])
+
+    np.testing.assert_array_equal(r["b"], [10, 20, 30])
+
+
+def test_setitem_by_int():
+    r = make_test_dynrec()
+    np.testing.assert_array_equal(r["a"], [3, 2, 1])
+    np.testing.assert_array_equal(r["b"], [10, 20, 30])
+
+    # setitem
+    r[1] = (4, 17)
+
+    np.testing.assert_array_equal(r["a"], [3, 4, 1])
+    np.testing.assert_array_equal(r["b"], [10, 17, 30])
